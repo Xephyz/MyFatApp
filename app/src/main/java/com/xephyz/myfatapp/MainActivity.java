6c5d43f47933dd234190445f10855ea149eaebe1
Anders <xephyzone@gmail.com>
2019-09-22 12:11:28 +0200

    Major name/id refactoring + new functionality
    Changed all names/ids to be easier to get when coding, made a naming convention for myself:
    View names start with:
    - TextView: txt<name>
    - Button: but<name>
    - ImageView: img<name>
    - EditText: input<name>
    - WebView: web<name>
    
    New functionality:
    - 3rd button "butLinkifyTxt" now changes the text in "txtChangeMe" and uses Linkify to add intents to the text
    
    TL;DR:
    - func: Linkify intents on text through button
    - new "naming convention"

--- app/src/main/java/com/xephyz/myfatapp/MainActivity.java
+++ app/src/main/java/com/xephyz/myfatapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.xephyz.myfatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
@@ -14,10 +15,10 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView welcomeText, currentTime, changeMeText;
-    ImageView welcomeIcon;
-    Button mainButt, butt2, butt3, urlOk;
-    EditText urlText;
+    TextView txtWelcome, txtCurrentTime, txtChangeMe;
+    ImageView imgWelcomeIcon;
+    Button butMain, butResetTime, butLinkifyTxt, butUrlOk;
+    EditText inputTxtUrl;
     WebView webView;
 
     @Override
@@ -26,49 +27,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         // TextViews
-        welcomeText = findViewById(R.id.welcome);
-        currentTime = findViewById(R.id.currentTime);
-        changeMeText = findViewById(R.id.changeMe);
+        txtWelcome = findViewById(R.id.txtWelcome);
+        txtCurrentTime = findViewById(R.id.txtCurrentTime);
+        txtChangeMe = findViewById(R.id.txtChangeMe);
 
         // Buttons
-        mainButt = findViewById(R.id.john);
-        butt2 = findViewById(R.id.button2);
-        butt3 = findViewById(R.id.button3);
-        urlOk = findViewById(R.id.urlOk);
+        butMain = findViewById(R.id.butMain);
+        butResetTime = findViewById(R.id.butResetTime);
+        butLinkifyTxt = findViewById(R.id.butLinkify);
+        butUrlOk = findViewById(R.id.butUrlOk);
 
         // ImageViews
-        welcomeIcon = findViewById(R.id.welcomeIcon);
+        imgWelcomeIcon = findViewById(R.id.imgWelcomeIcon);
 
         // EditTexts
-        urlText = findViewById(R.id.urlText);
+        inputTxtUrl = findViewById(R.id.inputTxtUrl);
 
         // WebViews
         webView = findViewById(R.id.webView);
 
-        mainButt.setText("Tap me!");
-        butt2.setText("Reset time");
-        urlText.setText("https://");
+        butMain.setText("Tap me!");
+        butResetTime.setText("Reset time");
+        inputTxtUrl.setText("https://");
         webView.loadUrl("https://javabog.dk");
 
         String timeText = "The time is:\n" + new Date();
-        currentTime.setText(timeText);
+        txtCurrentTime.setText(timeText);
 
-        mainButt.setOnClickListener(this);
-        butt2.setOnClickListener(this);
-        currentTime.setOnClickListener(this);
-        urlOk.setOnClickListener(this);
+        butMain.setOnClickListener(this);
+        butResetTime.setOnClickListener(this);
+        butLinkifyTxt.setOnClickListener(this);
+        txtCurrentTime.setOnClickListener(this);
+        butUrlOk.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v == currentTime) {
-            currentTime.setText("You... tapped the text?");
-        } else if (v == mainButt) {
-            currentTime.setText("Yay! You tapped the button!");
-        } else if (v == butt2) {
-            currentTime.setText("The time is:\n" + new Date());
-        } else if (v == urlOk) {
-            webView.loadUrl(urlText.getText().toString());
+        if (v == txtCurrentTime) {
+            txtCurrentTime.setText("You... tapped the text?");
+        } else if (v == butMain) {
+            txtCurrentTime.setText("Yay! You tapped the button!");
+        } else if (v == butResetTime) {
+            txtCurrentTime.setText("The time is:\n" + new Date());
+        } else if (v == butLinkifyTxt) {
+            txtChangeMe.setText("Linkify er cool: Mit telefonnummer er 20339944,\nmin e-post er xephyzone@gmail.com og\njeg har ikke en hjemmeside... Men jeg kan godt lide https://reddit.com.");
+            Linkify.addLinks(txtChangeMe, Linkify.ALL);
+        } else if (v == butUrlOk) {
+            webView.loadUrl(inputTxtUrl.getText().toString());
         }
     }
 }
