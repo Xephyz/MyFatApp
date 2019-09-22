f9c1cfb3a57ea32a48077c872825dbe4d8145e5c
Anders <xephyzone@gmail.com>
2019-09-22 13:45:25 +0200

    Fresh start
    Removed all functionality and buttons and such to start over fresh.
    Fresh start because i'll be trying out programmatic layout instead. :ok_hand:

--- app/src/main/java/com/xephyz/myfatapp/MainActivity.java
+++ app/src/main/java/com/xephyz/myfatapp/MainActivity.java
@@ -15,65 +15,13 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView txtWelcome, txtCurrentTime, txtChangeMe;
-    ImageView imgWelcomeIcon;
-    Button butMain, butResetTime, butLinkifyTxt, butUrlOk;
-    EditText inputTxtUrl;
-    WebView webView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // TextViews
-        txtWelcome = findViewById(R.id.txtWelcome);
-        txtCurrentTime = findViewById(R.id.txtCurrentTime);
-        txtChangeMe = findViewById(R.id.txtChangeMe);
-
-        // Buttons
-        butMain = findViewById(R.id.butMain);
-        butResetTime = findViewById(R.id.butResetTime);
-        butLinkifyTxt = findViewById(R.id.butLinkify);
-        butUrlOk = findViewById(R.id.butUrlOk);
-
-        // ImageViews
-        imgWelcomeIcon = findViewById(R.id.imgWelcomeIcon);
-
-        // EditTexts
-        inputTxtUrl = findViewById(R.id.inputTxtUrl);
-
-        // WebViews
-        webView = findViewById(R.id.webView);
-
-        butMain.setText("Tap me!");
-        butResetTime.setText("Reset time");
-        inputTxtUrl.setText("https://");
-        webView.loadUrl("https://javabog.dk");
-
-        String timeText = "The time is:\n" + new Date();
-        txtCurrentTime.setText(timeText);
-
-        butMain.setOnClickListener(this);
-        butResetTime.setOnClickListener(this);
-        butLinkifyTxt.setOnClickListener(this);
-        txtCurrentTime.setOnClickListener(this);
-        butUrlOk.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v == txtCurrentTime) {
-            txtCurrentTime.setText("You... tapped the text?");
-        } else if (v == butMain) {
-            txtCurrentTime.setText("Yay! You tapped the button!");
-        } else if (v == butResetTime) {
-            txtCurrentTime.setText("The time is:\n" + new Date());
-        } else if (v == butLinkifyTxt) {
-            txtChangeMe.setText("Linkify er cool: Mit telefonnummer er 20339944,\nmin e-post er xephyzone@gmail.com og\njeg har ikke en hjemmeside... Men jeg kan godt lide https://reddit.com.");
-            Linkify.addLinks(txtChangeMe, Linkify.ALL);
-        } else if (v == butUrlOk) {
-            webView.loadUrl(inputTxtUrl.getText().toString());
-        }
     }
 }
